897bc9e0b649306b5515d709a5d0735417ea91f2
arian-picco <apicco@sparkdigital.com>
2021-04-27 09:44:32 -0300

    functions

---
+++ function.py
@@ -0,0 +1,11 @@
+def my_function():
+    print('entraste en mi funcion')
+
+my_function()
+
+def sumar(primer_numero,segundo_numero):
+    suma = primer_numero + segundo_numero
+    print(suma)
+    return suma
+
+sumar(10,10)
